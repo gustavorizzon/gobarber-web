ac9ee3d5f3a97137617d715d5599d9a5f1acf605
gustavorizzon <gustavop.rizzon@gmail.com>
2020-04-25 00:16:27 -0300

    added authenticated routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <h1>Hello World</h1>
+  </Container>
+);
+
+export default Dashboard;
